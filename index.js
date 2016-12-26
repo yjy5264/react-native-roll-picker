234b05c515855aa91fa39d220fdd9e761882251a
余进洋 <yjy5264@sina.com>
2016-12-26 14:45:43 +0800

    change PickerSpace

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Created by yjy on 2016/12/26.
  */
 const Picker = require('./lib/Picker');
-const PickerSpace = require('./lib/Picker');
+const PickerSpace = require('./lib/PickerSpace');
 const CityPicker = require('./demo/CityPicker');
 
 module.exports = {Picker, PickerSpace, CityPicker};
